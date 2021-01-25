85d80a840ab6dec46f421f41047d4e603eac1396
Kate Tyshchenko <tyshchenko.kate@gmail.com>
2021-01-25 10:47:57 -0800

    Make spheres move

---
+++ CountingPrototype/MoveForward.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    private float speed = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+}
