0230b4c7dc1e5956352ab7386a838c1180b5d346
Kate Tyshchenko <tyshchenko.kate@gmail.com>
2021-01-25 10:41:23 -0800

    Set random cube scale

--- ModTheCube/Cube.cs
+++ ModTheCube/Cube.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
-    
+
+    private float limit = 3.0f; // limit to set position/scale
+
     void Start()
     {
         SetPosition();
+        SetScale();
     }
     
     void Update()
@@ -20,5 +23,10 @@ public class Cube : MonoBehaviour
     {
         Vector3 pos = new Vector3(Random.Range(0f, limit), limit * 2, 0.0f);
         transform.position = pos;
+    }
+
+    private void SetScale()
+    {
+        transform.localScale = Vector3.one * Random.Range(0.0f, limit);
     }
 }
