49277ea273659381e262d66bb359415b2ace325b
Kate Tyshchenko <tyshchenko.kate@gmail.com>
2021-01-25 11:01:53 -0800

    Text size change

--- MenuProject/GameManager.cs
+++ MenuProject/GameManager.cs
@@ -7,18 +7,37 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    private TMP_Text[] textComponents;
+    private Text[] textComponents2;
     public Slider audioSlider;
 
+    public Slider textSizeSlider;
     private AudioSource ac;
 
     private void Start()
     {
+        textComponents = Object.FindObjectsOfType<TMP_Text>();
+        textComponents2 = Object.FindObjectsOfType<Text>();
+
         GetAudio();
     }
 
     public void StartGame()
     {
         SceneManager.LoadScene("MainScene");
+    }
+
+    public void TextSizeChange()
+    {
+        foreach (TMP_Text texts in textComponents)
+        {
+            texts.fontSize = (int)textSizeSlider.value;
+        }
+
+        foreach (Text texts in textComponents2)
+        {
+            texts.fontSize = (int)textSizeSlider.value;
+        }
     }
 
     public void AudioVolumeChange()
