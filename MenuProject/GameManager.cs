a64631f611abdee8122fa037fcea311ee8d3a90a
Kate Tyshchenko <tyshchenko.kate@gmail.com>
2021-01-25 11:00:01 -0800

    Add audio volume fns

--- MenuProject/GameManager.cs
+++ MenuProject/GameManager.cs
@@ -7,13 +7,31 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public Slider audioSlider;
+
+    private AudioSource ac;
+
     private void Start()
     {
-       
+        GetAudio();
     }
 
     public void StartGame()
     {
         SceneManager.LoadScene("MainScene");
+    }
+
+    public void AudioVolumeChange()
+    {
+        if (ac != null)
+        {
+            ac.volume = audioSlider.value;
+        }
+    }
+
+    public void GetAudio()
+    {
+        ac = GameObject.Find("Main Camera").GetComponent<AudioSource>();
+        ac.volume = 0.5f;
     }
 }
