a5ef32415f5f8c7d36a0dd2697d41a3d2de57e36
Kate Tyshchenko <tyshchenko.kate@gmail.com>
2021-01-25 10:43:43 -0800

    Space key functionality

--- ModTheCube/Cube.cs
+++ ModTheCube/Cube.cs
@@ -21,6 +21,12 @@ public class Cube : MonoBehaviour
     void Update()
     {
         RotateCube();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SetPosition();
+            SetScale();
+        }
     }
 
     private void SetPosition()
