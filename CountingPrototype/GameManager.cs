9ce6abe0896d2c6d5ad0e99e7c62d6e21421a84f
Kate Tyshchenko <tyshchenko.kate@gmail.com>
2021-01-25 10:48:23 -0800

    Game duration and ball instant

---
+++ CountingPrototype/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject sphere;
+
+    private static float duration = 30.0f;
+    private static bool isGameOver = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isGameOver)
+        {
+            Instantiate(sphere);
+        }
+
+        StartCoroutine(GameCoroutine());
+
+    }
+
+    private IEnumerator GameCoroutine()
+    {
+        yield return new WaitForSeconds(duration);
+
+        isGameOver = true;
+    }
+
+
+}
