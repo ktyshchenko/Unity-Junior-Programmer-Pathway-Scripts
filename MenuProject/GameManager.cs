0c3c7a4807dcd1bee68cb0c38f4b9619e130d092
Kate Tyshchenko <tyshchenko.kate@gmail.com>
2021-01-25 10:56:06 -0800

    Add empty script

---
+++ MenuProject/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    private void Start()
+    {
+       
+    }
+
+    private void Update()
+    {
+
+    }
+}
