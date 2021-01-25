c09c8aaf2dfa7725eb5e12e11a2c2fca0a0c1ae6
Kate Tyshchenko <tyshchenko.kate@gmail.com>
2021-01-25 10:40:40 -0800

    Set random cube pos

--- ModTheCube/Cube.cs
+++ ModTheCube/Cube.cs
@@ -8,16 +8,17 @@ public class Cube : MonoBehaviour
     
     void Start()
     {
-        transform.position = new Vector3(3, 4, 1);
-        transform.localScale = Vector3.one * 1.3f;
-        
-        Material material = Renderer.material;
-        
-        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+        SetPosition();
     }
     
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+
+    }
+
+    private void SetPosition()
+    {
+        Vector3 pos = new Vector3(Random.Range(0f, limit), limit * 2, 0.0f);
+        transform.position = pos;
     }
 }
