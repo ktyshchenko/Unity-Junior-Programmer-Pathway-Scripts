82b418e2ed27f296cea88593ef438e9c6c7e9ef8
Kate Tyshchenko <tyshchenko.kate@gmail.com>
2021-01-26 18:14:26 -0800

    Modify pos&scale limits

--- ModTheCube/Cube.cs
+++ ModTheCube/Cube.cs
@@ -7,7 +7,7 @@ public class Cube : MonoBehaviour
     public MeshRenderer Renderer;
 
     public float speed = 15.0f;
-    private float limit = 3.0f; // limit to set position/scale
+    private float limit = 6.0f; // limit to set position/scale
     private float repeatTime = 2.0f;
 
     void Start()
@@ -31,13 +31,13 @@ public class Cube : MonoBehaviour
 
     private void SetPosition()
     {
-        Vector3 pos = new Vector3(Random.Range(0f, limit), limit * 2, 0.0f);
+        Vector3 pos = new Vector3(Random.Range(0.0f, limit), limit, Random.Range(-limit, limit));
         transform.position = pos;
     }
 
     private void SetScale()
     {
-        transform.localScale = Vector3.one * Random.Range(0.0f, limit);
+        transform.localScale = Vector3.one * Random.Range(0.0f, limit / 2);
     }
 
     private void SetColorCube()
